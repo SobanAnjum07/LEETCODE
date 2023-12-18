e2af0087786e4d82e9bade37a20b7b18be5109d2
Soban <msobananjum786@gmail.com>
2023-12-19 03:20:56 +0500

    code

---
+++ 5. Longest Palindromic Substring.py
@@ -0,0 +1,31 @@
+
+'''
+.########
+.##......
+.##......
+.#######.
+.......##
+.##....##
+..######.
+
+'''
+
+def check_palindrome(s):
+    pass
+    
+    
+def isPalindrome(s):
+    return s == s[::-1]
+    
+
+
+if __name__ == '__main__':
+    
+    string = input()
+    
+    for i in range(len(string)):
+        
+        for j in range(len(string)):
+            
+        
+
