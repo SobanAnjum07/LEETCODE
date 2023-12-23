4af0cde0309bef42aa7ab1c9f2b4d19ba9f946df
Soban <msobananjum786@gmail.com>
2023-12-24 00:01:17 +0500

    Update

--- 5. Longest Palindromic Substring.py
+++ 5. Longest Palindromic Substring.py
@@ -18,14 +18,35 @@ def isPalindrome(s):
     return s == s[::-1]
     
 
+def longest_palindrome(string):
+    # string = input()
+    starting = 0
+    len_pal = 0
+    
+    for _ in range(len(string)):
+        ending = len(string)
+        
+        for i in range(len(string)):
+            
+            if isPalindrome(string[starting:ending]) and len(string[starting:ending]) > 1 and len_pal < len(string[starting:ending]): 
+                len_pal = len(string[starting:ending])
+                ret_str = string[starting:ending]
+            
+            ending -=1
+            
+        starting += 1
+        
+    return ret_str
+    
+    
 
 if __name__ == '__main__':
+    s = 'kalaabcdcbaj'
+    # s = 'abcdcba'
+    print(longest_palindrome(s))
+    pass
     
-    string = input()
     
-    for i in range(len(string)):
-        
-        for j in range(len(string)):
-            
+                
         
 
