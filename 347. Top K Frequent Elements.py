b199c42d7601443d87bb19de0acc2f63099d9d8b
Soban <msobananjum786@gmail.com>
2024-03-07 23:04:14 +0500

    CODE

--- 347. Top K Frequent Elements.py
+++ 347. Top K Frequent Elements.py
@@ -1,6 +1,7 @@
 
 
 from collections import Counter
+
 def topKFrequent(nums: list[int], k: int) -> list[int]:
         
     res = Counter(nums)
@@ -15,17 +16,8 @@ def topKFrequent(nums: list[int], k: int) -> list[int]:
     
     formed = []
     for i in range(k):
+
         formed.append(now[i][0])
     
     return formed
         
-        
-        
-        
-if __name__ == "__main__":
-    nums = [1,1,1,2,2,3]
-    nums = [3,0,1,0]
-    k = 1
-    
-    print(topKFrequent(nums, k))
-    
