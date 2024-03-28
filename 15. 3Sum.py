cf1923fb5279faaa66b6de8a0b938111dfc34228
Soban <msobananjum786@gmail.com>
2024-03-29 00:33:41 +0500

    CODE

--- 15. 3Sum.py
+++ 15. 3Sum.py
@@ -1,23 +1,24 @@
-# class Solution:
-def threeSum(self, nums: list[int]) -> list[list[int]]:
-    res = []
-    nums.sort()
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
 
-    for i, a in enumerate(nums):
-        if i > 0 and a == nums[i - 1]:
-            continue
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i - 1]:
+                continue
 
-        left, right = i + 1, len(nums) - 1
+            left, right = i + 1, len(nums) - 1
 
-        while left < right:
-            t_s = a + nums[left] + nums[right]
-            if t_s > 0:
-                right -= 1
-            elif t_s < 0:
-                left += 1
-            else:
-                res.append([a, nums[left], nums[right]])
-
-                left += 1
-                while nums[left] == nums[left - 1] and left < right:
+            while left < right:
+                t_s = a + nums[left] + nums[right]
+                if t_s > 0:
+                    right -= 1
+                elif t_s < 0:
                     left += 1
+                else:
+                    res.append([a, nums[left], nums[right]])
+
+                    left += 1
+                    while nums[left] == nums[left - 1] and left < right:
+                        left += 1
+        return res
