0ae1edd475f2fa5e621c2120dc75268a40c00e56
Soban <msobananjum786@gmail.com>
2024-09-15 12:34:52 +0500

    CODE

--- 5. Longest Palindromic Substring.py
+++ 5. Longest Palindromic Substring.py
@@ -9,43 +9,22 @@
 ..######.
 
 '''
-
-def check_palindrome(s):
-    pass
-    
-    
-def isPalindrome(s):
-    return s == s[::-1]
-    
-
-def longest_palindrome(string):
-    # string = input()
-    starting = 0
-    len_pal = 0
-    
-    for _ in range(len(string)):
-        ending = len(string)
-        
-        for i in range(len(string)):
-            
-            if isPalindrome(string[starting:ending]) and len(string[starting:ending]) > 1 and len_pal < len(string[starting:ending]): 
-                len_pal = len(string[starting:ending])
-                ret_str = string[starting:ending]
-            
-            ending -=1
-            
-        starting += 1
-        
-    return ret_str
-    
-    
-if __name__ == '__main__':
-    s = 'kalaabcdcbaj'
-    # s = 'abcdcba'
-    print(longest_palindrome(s))
-    pass
-    
-    
-                
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
         
 
+        def check_palindrome(string):
+
+            return string[::-1] == string
+
+        temp = ""
+        for i in range(len(s)):
+
+            for j in range(len(s), i, -1):
+
+                # print(s[i:j])
+
+                if check_palindrome(s[i:j]) and len(temp) < len(s[i:j]):
+                    temp = s[i:j]
+
+        return temp
